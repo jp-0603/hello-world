5fa2f04c17a3da732da475b509a6e2f8d3440e7e
jp-0603 <jparedes0303@gmail.com>
2018-11-08 17:32:41 -0500

    Complete

--- functions.js
+++ functions.js
@@ -1,3 +1,14 @@
 function 	HelloConsole() {
   console.log("Hello, World!");
 }
+function popup() {
+  alert("Hello, World!");
+}
+function SayHello() {
+  var p = document.getElementById("browser");
+  p.innerHTML ="Hello, World!";
+}
+function helloStranger() {
+  var name = prompt("Please Enter Your First Name");
+  alert("Hello " + name);
+}
